87dfec1d6df7667ff6aee29b4e249acafc981b52
vinnikoff <winr7post@gmail.com>
2016-12-12 17:14:11 +0200

    project2-1

---
+++ ArraysUtils.java
@@ -0,0 +1,114 @@
+/**
+ * Created by VINNI on 12.12.16.
+ */
+public class ArraysUtils {
+
+    public static final void min(int[] arr) {
+
+        int min = arr[0];
+        for (int i = 1; i <= arr.length - 1; ++i) {
+            if (arr[i] < min) min = arr[i];
+        }
+    }
+
+
+    public static final void max(int[] arr) {
+
+        int max = arr[0];
+        for (int i = 1; i <= arr.length - 1; ++i) {
+            if (arr[i] > max) max = arr[i];
+        }
+
+    }
+
+
+    public static final void sum(int[] arr) {
+        int sum1 = 0;
+        for (int i = 0; i <= arr.length - 1; i++) {
+
+            sum1 = sum1 + arr[i];
+        }
+
+    }
+
+    public static final void multiplication(int[] arr) {
+        int mult = 1;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            mult = mult * arr[i];
+        }
+
+    }
+
+
+    public static final void maxPositive(int[] arr) {
+
+        int max = arr[0];
+        for (int i = 1; i <= arr.length - 1; ++i) {
+            if (arr[i] > max) max = arr[i];
+        }
+        if (max <= 0) max = 0;
+    }
+
+
+    public static final void modulus(int[] arr) {
+        int first = arr[0];
+        int last = arr[arr.length - 1];
+        int mod = first % last;
+    }
+
+
+    public static final void secondLagest(int[] arr) {
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+
+        for (int z = arr.length - 1; z > 0; z--) {
+            if (arr[z] != arr[z - 1]) {
+
+                break;
+            }
+        }
+    }
+
+    public static final int[] reverse(int[] arr2) {
+
+        int tmp;
+        for (int i = 0; i < arr2.length / 2; i++) {
+            tmp = arr2[arr2.length - 1 - i];
+            arr2[arr2.length - 1 - i] = arr2[i];
+            arr2[i] = tmp;
+        }
+
+        return arr2;
+    }
+
+    public static final int[] findEvenElements(int[] arr2) {
+        boolean mask[] = new boolean[arr2.length];
+        int removeCount = 0;
+
+        for (int j = 0; j < arr2.length; j++) {
+            if (arr2[j]% 2 != 0 || arr2[j] ==0) {
+                mask[j] = true;
+                removeCount++;
+            }
+        }
+
+        int[] result = new int[arr2.length - removeCount];
+
+        for (int i = 0, j = 0; i < arr2.length; i++) {
+            if (!mask[i]) {
+                result[j++] = arr2[i];
+            }
+        }
+
+        return result;
+    }
+}
--- Main.java
+++ Main.java
@@ -2,166 +2,19 @@ import java.util.*;
 
 public class Main
 {
-    public static void main(String[] args)
-    {
-        int arr[] = {7, 7, -5, 0,-33,-5,-4, 1,-33,-1};
-        int arrravno=0;
-        for(int z=0; z< arr.length-1; z++)
-        {
-            if (arr[z] != arr[z+1])
-                arrravno=1;
-        }
+    public static void main(String[] args) {
+        int arr1[] = {7, 7, -5, 0, -33, -5, -4, 1, -33, 2};
 
-        System.out.println("Исходный массив:");
-        System.out.println(Arrays.toString(arr));
-/*for(int k=0; k <=l; k++)
-{
-System.out.print(arr[k]+" ");
-}*/
-
-/*for(int i = l ; i > 0 ; i--)
-{
-for(int j = 0 ; j < i ; j++)
-{
-
-if( arr[j] > arr[j+1] ){
-int tmp = arr[j];
-arr[j] = arr[j+1];
-arr[j+1] = tmp;
-}
-}
-}*/
-
-        System.out.println("\n");
-        sum(arr);
-        System.out.println("\n");
-        min(arr, arrravno);
-        System.out.println("\n");
-        max(arr, arrravno);
-        System.out.println("\n");
-        maxPositive(arr, arrravno);
-        System.out.println("\n");
-        multiplication(arr);
-        System.out.println("\n");
-        modulus(arr);
-        System.out.println("\n");
-        secondLagest(arr, arrravno);
+        ArraysUtils.sum(arr1);
+        ArraysUtils.min(arr1);
+        ArraysUtils.max(arr1);
+        ArraysUtils.maxPositive(arr1);
+        ArraysUtils.multiplication(arr1);
+        ArraysUtils.modulus(arr1);
+        ArraysUtils.reverse(arr1);
+        ArraysUtils.findEvenElements(arr1);
+        ArraysUtils.secondLagest(arr1);
 
     }
 
-    public static void min(int[] arr, int arrravno)
-    {
-        if (arrravno==0)
-            System.out.println("min: все эл. массива равны");
-        else
-        {
-            int min = arr[0];
-            for (int i=1; i<= arr.length-1; ++i)
-            {
-                if(arr[i]<min) min=arr[i];
-            }
-            System.out.println("min:"+min);
-        }
-    }
-
-    public static void max(int[] arr,int arrravno)
-    {
-        if (arrravno==0)
-            System.out.println("max: все эл. массива равны");
-        else
-        {
-            int max=arr[0];
-            for (int i=1; i<= arr.length-1; ++i)
-            {
-                if(arr[i]>max) max=arr[i];
-            }
-
-            System.out.println("max:"+max);
-        }
-    }
-    public static void sum(int[] arr)
-    {
-        int sum1 = 0;
-        for(int i = 0; i <= arr.length-1; i++){
-
-            sum1=sum1+arr[i];}
-
-        System.out.println("сумма элементов:"+sum1);
-
-    }
-
-    public static void multiplication(int[] arr)
-    {
-        int mult = 1;
-        for(int i = 0; i <= arr.length-1 ; i++)
-        {
-            mult=mult*arr[i];
-        }
-        System.out.println("multiplication:"+mult);
-    }
-
-
-    public static void maxPositive(int[] arr, int arrravno)
-    {
-        if (arrravno==0)
-            System.out.println("maxPositive: все эл. массива равны");
-        else
-        {
-            int max=arr[0];
-            for (int i=1; i<= arr.length-1; ++i)
-            {
-                if(arr[i]>max) max=arr[i];
-            }
-            if (max>0)
-            {
-                System.out.println("maxPositive:"+max);
-            }
-            else System.out.println("Положительные элементы отсутствуют");
-        }
-    }
-
-    public static void modulus(int[] arr)
-    {
-        int first=arr[0];
-        int last=arr[arr.length-1];
-        if (last !=0)
-        {
-
-            int mod=first%last;
-            System.out.println("modulus первого и последн. эл.:"+mod);
-        }
-        else System.out.println("modulus: Последний эл. равен нулю");
-    }
-
-
-    public static void secondLagest(int[] arr, int arrravno)
-    {
-        if (arrravno==0)
-            System.out.println("secondLagest: все эл. массива равны");
-        else
-        {
-            for(int i = arr.length-1; i > 0 ; i--)
-            {
-                for(int j = 0 ; j < i ; j++)
-                {
-
-                    if( arr[j] > arr[j+1] ){
-                        int tmp = arr[j];
-                        arr[j] = arr[j+1];
-                        arr[j+1] = tmp;
-                    }
-                }
-            }
-
-
-            for(int z= arr.length-1; z >0; z--)
-            {
-                if (arr[z] != arr[z-1])
-                {
-                    System.out.println("secondLagest:"+arr[z-1]);
-                    return;
-                }
-            }
-        }
-    }
 }
